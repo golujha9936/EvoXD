a9cebaf89b1fee6b6a65a7ca4da653a333c4795b
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2024-01-01 22:23:35 +0530

    Update settings.py

--- HarleyMusic/plugins/bot/settings.py
+++ HarleyMusic/plugins/bot/settings.py
@@ -76,12 +76,13 @@ async def gib_repo(client, CallbackQuery, _):
     ), 
     return await CallbackQuery.edit_message_text( 
         reply_markup=InlineKeyboardMarkup( 
-        [  
+ [
             InlineKeyboardButton(text="Uᴘᴅᴀᴛᴇꜱ", url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT),
-        ], 
-        [InlineKeyboardButton(text="◁", callback_data=f"settingsback_helper"),
-        ] 
+            InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT"),
+        ],
+        [
+           InlineKeyboardButton(text="◁", callback_data=f"settingsback_helper"),
+        ],
         ), 
     )
 
